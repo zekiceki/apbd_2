218bec0520f6c1cb2c0b3966045ddf2fd646233a
s25817 <s25817@pjwstk.edu.pl>
2024-03-12 11:41:00 +0100

    Added static method to find maximum value

---
+++ .idea/max.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class Calculator
+{
+    public static int FindMax(int[] numbers)
+    {
+        if (numbers.Length == 0)
+        {
+            throw new ArgumentException("Array must not be empty");
+        }
+
+        int max = numbers[0];
+        foreach (int num in numbers)
+        {
+            if (num > max)
+            {
+                max = num;
+            }
+        }
+        return max;
+    }
+
+    public static void Main(string[] args)
+    {
+        int[] numbers = { 1, 5, 3, 7, 2 };
+        Console.WriteLine("Maximum value: " + FindMax(numbers));
+    }
+}
