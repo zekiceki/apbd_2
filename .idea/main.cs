8af591a43789f67c8f6e956464e2153f36f3f18e
s25817 <s25817@pjwstk.edu.pl>
2024-03-12 11:25:48 +0100

    Added static method to calculate average

--- .idea/main.cs
+++ .idea/main.cs
@@ -1,6 +1,18 @@
-﻿namespace DefaultNamespace;
+﻿public class Calculator {
+    public static double calculateAverage(int[] numbers) {
+        if (numbers.length == 0) {
+            throw new IllegalArgumentException("Array must not be empty");
+        }
+        
+        int sum = 0;
+        for (int num : numbers) {
+            sum += num;
+        }
+        return (double) sum / numbers.length;
+    }
 
-public class main
-{
-    sxdrcftvgybhunjimko,lp.;[/''] ashdshadasksdasdakxbhdbjdbdbajhkdsbajksbajk `21mkl1msadasdasdsadsadsadsadsadaxhkjnaslsmak;ls,async.a;
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        System.out.println("Average: " + calculateAverage(numbers));
+    }
 }
